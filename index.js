40b8dbc6d2bac7b7916385acb8de63609f91c4b8
Luciano <lucho.2012.tandil@gmail.com>
2024-01-16 17:23:14 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const sgMail = require('@sendgrid/mail')
+require("dotenv").config({ path: '.env' })
+const express = require('express')
+const bodyParser = require("body-parser")
+
+const app = express()
+app.use(express.json())
+
+app.use(bodyParser.json())
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+app.post('/', function requestHandler(req, res) {
+    const PARAMETROS = req.body
+    console.log(PARAMETROS)
+
+    const msg = {
+        to: PARAMETROS.to, 
+        from: 'registro.gfc@greenborn.com.ar', 
+        subject: PARAMETROS.subject,
+        text: PARAMETROS.text,
+        html: PARAMETROS.html,
+    }
+    sgMail
+        .send(msg)
+        .then(() => {
+            res.status(200).send({ stat: true })
+        })
+        .catch((error) => {
+            res.status(200).send({ stat: false })
+        })
+        
+    
+});
+
+setTimeout(async () => {
+    const server = await app.listen(process.env.PUERTO);
+    console.log('Servidor escuchando en: ',process.env.PUERTO) 
+}, 100)
